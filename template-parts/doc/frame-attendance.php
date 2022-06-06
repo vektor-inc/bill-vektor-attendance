40b3141db5ac0cf7ffd5fba360568b91efbcc32c
kurudrive <kurudrive@gmail.com>
2022-06-06 13:49:57 +0900

    fix : 余計なパラメーター表示削除

--- template-parts/doc/frame-attendance.php
+++ template-parts/doc/frame-attendance.php
@@ -33,7 +33,7 @@
 					$table .= '<tr class="bg-info">';
 				} else {
 					if ( isset( $table_data[ $i ]['holiday'] ) && 'koukyuu' === $table_data[ $i ]['holiday'] ) {
-						$table .= '<tr class="bg-warning">  ' . $table_data[ $i ]['holiday'];
+						$table .= '<tr class="bg-warning">';
 					} else {
 						$table .= '<tr>';
 					}
